6bae05d7cf6c5525bc605af38f6fd41358265a83
Myohmtechnologies <marketing@myohmtechnologies.com>
2025-01-29 11:42:47 +0100

    payments from stripe dashbaord3

--- src/app/api/payments/route.ts
+++ src/app/api/payments/route.ts
@@ -1,13 +1,5 @@
 import { NextResponse } from 'next/server'
-import Stripe from 'stripe'
-
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not defined')
-}
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-01-27.acacia'
-})
+import { stripe } from '@/lib/stripe'
 
 export const dynamic = 'force-dynamic'
 
